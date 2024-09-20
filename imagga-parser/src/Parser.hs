122f97ac25abd51213a1330db57881903e547601
Matthew-Mosior <mattm.github@gmail.com>
2024-09-20 10:42:52 -0400

    Updating imagga-parser.

--- imagga-parser/src/Parser.hs
+++ imagga-parser/src/Parser.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Parser where
 
@@ -37,12 +38,19 @@ data Status = Status
   , type_ :: Text  -- 'type' is a reserved word in Haskell, so we use 'type_'
   } deriving (Show, Generic)
 
-instance FromJSON Status
-instance ToJSON Status
+instance FromJSON Status where
+  parseJSON = withObject "Status" $ \v -> Status
+    <$> v .: "text"
+    <*> v .: "type"
+
+instance ToJSON Status where
+  toJSON (Status txt typ) =
+    object ["text" .= txt, "type" .= typ]
 
 -- Main data type to capture the overall JSON structure
 data ApiResponse = ApiResponse
   { result :: Result
+  , status :: Status
   } deriving (Show, Generic)
 
 instance FromJSON ApiResponse
